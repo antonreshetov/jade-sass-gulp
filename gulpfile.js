57bc57a9f902a76b5001e6f61c8d4e08db149494
antonreshetov <reshetov.art@gmail.com>
2016-03-29 08:15:52 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var watch        = require('gulp-watch');
 var useref       = require('gulp-useref');
 var clean        = require('gulp-clean');
 var merge        = require('gulp-merge');
+var jshint       = require('gulp-jshint');
+var uglify       = require('gulp-uglify');
+var rename       = require("gulp-rename");
+var concat       = require("gulp-concat");
+var order        = require("gulp-order");
 
 // Browser Sync
 gulp.task('browserSync', function() {
@@ -32,8 +37,15 @@ gulp.task('copyfiles', function() {
 // SCSS to CSS + Prefix
 gulp.task('css', function() {
   return gulp.src('src/assets/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 15 versions']}))
+    .pipe(gulp.dest('app/assets/css'))
+    // compressed
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({browsers: ['last 15 versions']}))
+    .pipe(rename({
+      suffix: '.min'
+      }))
     .pipe(gulp.dest('app/assets/css'))
     .pipe(browserSync.reload({stream:true}));
 });
@@ -49,10 +61,44 @@ gulp.task('jade', function(){
     .pipe(browserSync.reload({stream:true}));
 });
 
+// Js
+gulp.task('jsConcat', ['jsMain'], function() {
+  return gulp.src('./src/assets/js/lib/*.js')
+    .pipe(order([
+      "src/assets/js/lib/jquery-1.11.2.min.js",
+      "src/assets/js/lib/*.js"
+    ],{ base: './' }))
+    .pipe(concat("bundle.js"))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+      }))
+    .pipe(gulp.dest('app/assets/js'));
+});
+
+gulp.task('jsMain',function(){
+  return gulp.src('./src/assets/js/main.js')
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+      }))
+    .pipe(gulp.dest('app/assets/js'));
+});
+
+// JsHint
+gulp.task('lint', function() {
+  return gulp.src('src/assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
 // Watch
 gulp.task('watch', function(){
   gulp.watch('src/assets/scss/**/*.scss',['css']);
   gulp.watch('src/jade/**/*.jade',['jade']);
+  gulp.watch('src/assets/js/main.js',['jsMain']);
 });
 
 // Clean app
@@ -62,4 +108,4 @@ gulp.task('clean', function () {
 });
 
 // Run Default
-gulp.task('default',['browserSync', 'css', 'jade','copyfiles', 'watch']);
+gulp.task('default',['browserSync', 'css', 'jade', 'jsConcat', 'copyfiles', 'watch']);
